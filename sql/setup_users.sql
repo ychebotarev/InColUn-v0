1ee55df576899c2e74b23e7e64c6bc8c74a702ee
ychebotarev <ychebotarev@gmail.com>
2016-05-10 23:51:25 -0700

    boards

--- sql/setup_users.sql
+++ sql/setup_users.sql
@@ -8,31 +8,24 @@ CREATE TABLE IF NOT EXISTS `incolun`.`users` (
   `username` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
-  `type` CHAR(1) NOT NULL,
-  `created` DATETIME NOT NULL,
-  `status` CHAR(1) NOT NULL,
-  `kudos` INT UNSIGNED NOT NULL DEFAULT 0,
+  `type` ENUM('L', 'F', 'G') NOT NULL,
+  `created` DATETIME NOT NULL DEFAULT NOW(),
+  `status` ENUM('N', 'A', 'D') NOT NULL DEFAULT 'N',
+  `kudos` INT UNSIGNED NULL DEFAULT 0,
   `json_blob` JSON NULL,
-  PRIMARY KEY (`id`, `profile_id`))
+  PRIMARY KEY (`id`, `profile_id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
 ENGINE = InnoDB;
 
+INSERT INTO incolun.users (id,profile_id, username, email, password, type) 
+   VALUES (0, '1372734638','a','a@a','2ebb8efcaa00598520e7b4fdc7d3a6630bcb13f0','L');
 
-INSERT INTO incolun.users (id,profile_id, username, email, password, type, created, status) 
-   VALUES (0, '1372734638','a','a@a','2ebb8efcaa00598520e7b4fdc7d3a6630bcb13f0','L',NOW(),'A');
-
-INSERT INTO incolun.users (id,profile_id, username, email, password, type, created, status) 
-   VALUES (1, '4232722471','b','b@b','5ece1dc5bb7a19d0d5d8dc670a582519f5f3170a','L',NOW(),'A');
-
+INSERT INTO incolun.users (id,profile_id, username, email, password, type) 
+   VALUES (1, '4232722471','b','b@b','5ece1dc5bb7a19d0d5d8dc670a582519f5f3170a','L');
    
 SELECT * FROM incolun.users;
-
 SELECT * from incolun.users WHERE user_id_key = '1372734638';
 
-INSERT INTO incolun.users (id,user_id_key,user_id, email,username, password,token,type,created,status) 
-   VALUES (1, '1','b@b','b@b','b','N/A','NA','L',NOW(),'A');
-
-DELETE from incolun.users WHERE user_id_key=4232722471
-DELETE from incolun.users WHERE user_id_key=834624741
+DELETE from incolun.users WHERE user_id_key=4232722471;
+DELETE from incolun.users WHERE user_id_key=834624741;
                    
-INSERT INTO users (id,user_id_key,user_id, email,username, password, token, type,created,status) 
-	VALUES ('281779587464273920', '4232722471','b@b','b@b','b','5ece1dc5bb7a19d0d5d8dc670a582519f5f3170a','NA','L',NOW(),'N')
